9dee3dc90250f5abc9b976c4deff70c5c83d6cc8
rmendoza <rmendoza@riu.com>
2025-10-31 14:57:25 -0400

    fix: update node version to 20.x and skip linting temporarily

---
+++ javascript-api-automation/.eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  env: {
+    node: true,
+    es2021: true
+  },
+  extends: ['eslint:recommended'],
+  rules: {
+    'no-unused-vars': 'warn',
+    'no-console': 'off'
+  }
+};
